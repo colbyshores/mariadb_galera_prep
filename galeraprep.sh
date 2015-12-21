069daef080b0ed5725a361491092b388f15c08c3
Colby Shores <col2037184@maricopa.edu>
2015-12-21 10:31:07 -0700

    Update galeraprep.sh

--- galeraprep.sh
+++ galeraprep.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
+echo "" > altertable.log
 DATABASES=$(mysql -ANe"SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('mysql','information_schema','performance_schema')")
 for D in $DATABASES
 do
-  TABLES=$(mysql -p$2 -u$1 --skip-column-names -B -D $D -e 'show tables')
+  TABLES=$(mysql --skip-column-names -B -D $D -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '$D' AND engine = 'MyISAM';")
   for T in $TABLES
   do
-    echo "Processing $T in $D"
-    mysql -p$2 -u$1 -D $D -e "SET FOREIGN_KEY_CHECKS=0;ALTER TABLE $T ENGINE=INNODB;SET FOREIGN_KEY_CHECKS=1;"
+    echo "Processing $T in $D" >> altertable.log
+    mysql -e "SET FOREIGN_KEY_CHECKS=0;" >> altertable.log
+    mysql -D $D -e "ALTER TABLE $T ENGINE=INNODB;" >> altertable.log
+    mysql -e "SET FOREIGN_KEY_CHECKS=1;" >> altertable.log
+    mysql -e "use $D;show table status like '$T'\G;" >> altertable.log
   done
 done
